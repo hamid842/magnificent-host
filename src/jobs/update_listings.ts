82363b50137eca7c942de358bda5458b1dbfdc8d
Behzad Barzin <Behzad_Barzin@live.com>
2023-02-01 11:21:41 +0330

    Auto import data into DB from Hostaway data

--- src/jobs/update_listings.ts
+++ src/jobs/update_listings.ts
@@ -3,30 +3,35 @@ import { TCalendar, TListing } from "../utils/APITypes";
 
 export default {
   /**
-   * Run this job every 1 Minute(s)
-   */
+  * Shallow Search: Just see if the id of the listing exists, if not create it
+  * Run this job every 1 Minute(s)
+  */
   '*/1 * * * *': async ({ strapi }) => {
     //-------------------------------------------------------------------------
     console.log('Retrieve all listings: ');
+    const propertyUUID = 'api::property.property';
     const listings: TListing[] = await HostawayAPI.getListings();
     for (const listing of listings) {
-      console.log(`${listing.id} → ${listing.name}`);
+      const entry = await strapi.entityService.findOne(propertyUUID, listing.id);
+      if (!entry) {
+        const newEntry = await strapi.entityService.create(propertyUUID, {
+          data: {
+            id: listing.id,
+            Title: listing.name,
+            location: listing.address,
+            generalInformation: {},
+          },
+        });
+      }
     }
     //-------------------------------------------------------------------------
-    console.log('===================================');
-    console.log('Retrieve one listing: ');
-    const listing: TListing = await HostawayAPI.getListing(listings[1].id);
-    console.log(`${listing.id} → ${listing.name}`);
-    //-------------------------------------------------------------------------
-    console.log('===================================');
-    console.log('Retrieve one Calendar: ');
-    const calendars: TCalendar[] = await HostawayAPI.getCalendar(
-      listings[1].id,
-      new Date('2023-01-31'),
-      new Date('2023-02-3')
-    );
-    for (const calendar of calendars) {
-      console.log(`${calendar.date} → ${calendar.isAvailable}`);
-    }
+  },
+
+  /**
+  * Deep Comparison: See if Property items need any updates
+  * Run this job every 60 Minute(s)
+  */
+  '*/60 * * * *': async ({ strapi }) => {
+    console.log('Deep Comparison!');
   },
 };
