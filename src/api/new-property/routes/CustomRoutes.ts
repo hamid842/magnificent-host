98f9026a4f1dbd45023e7ac16068959733c036dc
Behzad Barzin <Behzad_Barzin@live.com>
2023-02-07 17:42:14 +0330

    BUGFIX: Mistake in NewProperty Router

--- src/api/new-property/routes/CustomRoutes.ts
+++ src/api/new-property/routes/CustomRoutes.ts
@@ -3,7 +3,7 @@ export default {
     { // Path defined with a URL parameter
       method: 'POST',
       path: '/new-properties/:id/approve',
-      handler: 'property.approve',
+      handler: 'new-property.approve',
     },
   ]
 }
