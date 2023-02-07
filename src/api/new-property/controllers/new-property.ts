6b6d34fb1a44c513b9c6d7e18fb94ceae8eb5c4f
Behzad Barzin <Behzad_Barzin@live.com>
2023-02-07 17:31:03 +0330

    Created NewProperty for 'Rent Your Place'

---
+++ src/api/new-property/controllers/new-property.ts
@@ -0,0 +1,7 @@
+/**
+ * new-property controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::new-property.new-property');
---
+++ src/api/new-property/routes/new-property.ts
@@ -0,0 +1,7 @@
+/**
+ * new-property router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::new-property.new-property');
