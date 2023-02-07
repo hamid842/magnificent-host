b2b99ba75fcd03c22fefc7af92204deaf2024a65
Behzad Barzin <Behzad_Barzin@live.com>
2023-02-07 17:38:17 +0330

    Placeholder logic for NewProperty approval (Post to Hostaway) added

--- src/api/new-property/controllers/new-property.ts
+++ src/api/new-property/controllers/new-property.ts
@@ -4,4 +4,16 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::new-property.new-property');
+export default factories.createCoreController('api::new-property.new-property', ({ strapi }) =>  ({
+  /**
+   * Custom controller action to Approve a NewProperty and Post it to Hostaway
+   * POST - /new-properties/:id/approve
+   */
+  async approve(ctx) {
+    // TODO: MUST COMPLETE
+    return 'NOT YET';
+  },
+
+  //=======================================================================================================
+
+}));
---
+++ src/api/new-property/routes/CustomRoutes.ts
@@ -0,0 +1,9 @@
+export default {
+  routes: [
+    { // Path defined with a URL parameter
+      method: 'POST',
+      path: '/new-properties/:id/approve',
+      handler: 'property.approve',
+    },
+  ]
+}
