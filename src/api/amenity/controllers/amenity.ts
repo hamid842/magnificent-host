98d99a11ef396e529466590818797ff0f96bf2eb
Behzad Barzin <Behzad_Barzin@live.com>
2023-02-25 11:13:15 +0330

    TEMP: Customize amenity controller to remove span tag from icon

--- src/api/amenity/controllers/amenity.ts
+++ src/api/amenity/controllers/amenity.ts
@@ -4,7 +4,46 @@
 
 import { factories } from '@strapi/strapi'
 
+// TODO: Temp code (remove when database is finalized)
+const cleanIcon = (amenity) => {
+  let icon: string = amenity.attributes.icon;
+  if (!icon) return;
+
+  icon = icon.replace('<span class="material-symbols-outlined">', '');
+  icon = icon.replace('</span>', '');
+  icon = icon.trim();
+  amenity.attributes.icon = icon;
+};
+
 export default factories.createCoreController('api::amenity.amenity', ({ strapi }) =>  ({
+  //=======================================================================================================
+  // TODO: Temp Code
+  /**
+  * Wrap core controller in custom logic
+  */
+  async findOne(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.findOne(ctx);
+
+    cleanIcon(data);
+
+    return { data, meta };
+  },
+  //=======================================================================================================
+  // TODO: Temp Code
+  /**
+   * Wrap core controller in custom logic
+   */
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    for (const amenity of data) {
+      cleanIcon(amenity);
+    }
+
+    return { data, meta };
+  },
   //=======================================================================================================
 
   /**
@@ -14,8 +53,17 @@ export default factories.createCoreController('api::amenity.amenity', ({ strapi 
     async getAll(ctx) {
       const amenities = await strapi.entityService.findMany('api::amenity.amenity');
       const slim = amenities.map((item) => {
-        return { id: item.id, name: item.name };
+        return { id: item.id, name: item.name, category: item.category, icon: item.icon };
       })
+      // TODO: Temp Code
+      for (const amenity of slim) {
+        let icon: string = amenity.icon;
+        if (!icon) continue;
+        icon = icon.replace('<span class="material-symbols-outlined">', '');
+        icon = icon.replace('</span>', '');
+        icon = icon.trim();
+        amenity.icon = icon;
+      }
       return slim;
     },
 
